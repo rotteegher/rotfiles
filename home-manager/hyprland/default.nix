5dde3c883c4a22d5cb6d231e9773a1ecbc8af326
Sawako Karin <noisetide@proton.me>
2023-12-12 04:28:07 +1100

    Hyprland minimal configured.

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -8,5 +8,183 @@
 }: let
   displays = config.rot.displays;
   in {
-   # TODO!
+    imports = [
+      # ./hyprnstack.nix
+      ./keybinds.nix
+      # ./lock.nix
+      # ./screenshot.nix
+      ./startup.nix
+      # ./wallpaper.nix
+      # ./waybar
+    ];
+
+    config = lib.mkIf config.wayland.windowManager.hyprland.enable {
+      home = {
+        sessionVariables = {
+          # XCURSOR_SIZE = "${toString config.home.pointerCursor.size}";
+          HYPR_LOG = "/tmp/hypr/$(command ls -t /tmp/hypr/ | grep -v lock | head -n 1)/hyprland.log";
+        };
+
+        packages = with pkgs; [
+          # clipboard history
+          cliphist
+          wl-clipboard
+        ];
+      };
+
+      wayland.windowManager.hyprland.settings = lib.mkMerge [
+        {
+          monitor =
+            (lib.forEach displays ({
+              name,
+              hyprland,
+              ...
+            }: "${name}, ${hyprland}"))
+            ++ (lib.optional (host != "desktop") ",preferred,auto,auto");
+
+          input = {
+            kb_layout = "jp";
+            follow_mouse = 1;
+
+            touchpad = {
+              natural_scroll = false;
+              disable_while_typing = true;
+            };
+          };
+
+          # "$mod" = "SUPER";
+          "$mod" = "ALT";
+
+          "$term" = "${config.rot.terminal.exec}";
+
+          general = let
+            gap =
+              if host == "desktop"
+              then 8
+              else 4;
+          in {
+            gaps_in = gap;
+            gaps_out = gap;
+            border_size = 2;
+            layout = "master";
+          };
+
+          decoration = {
+            rounding = 4;
+            drop_shadow = host != "vm";
+            shadow_range = 4;
+            shadow_render_power = 3;
+            "col.shadow" = "rgba(1a1a1aee)";
+
+            # dim_inactive = true
+            # dim_strength = 0.05
+
+            blur = {
+              enabled = host != "vm";
+              size = 2;
+              passes = 3;
+              new_optimizations = true;
+            };
+
+            # blurls = rofi
+          };
+
+          animations = {
+            enabled = host != "vm";
+            bezier = [
+              "overshot, 0.05, 0.9, 0.1, 1.05"
+              "smoothOut, 0.36, 0, 0.66, -0.56"
+              "smoothIn, 0.25, 1, 0.5, 1"
+            ];
+
+            animation = [
+              "windows, 1, 5, overshot, slide"
+              "windowsOut, 1, 4, smoothOut, slide"
+              "windowsMove, 1, 4, smoothIn, slide"
+              "border, 1, 5, default"
+              "fade, 1, 5, smoothIn"
+              "fadeDim, 1, 5, smoothIn"
+              "workspaces, 1, 6, default"
+            ];
+          };
+
+          dwindle = {
+            pseudotile = true;
+            preserve_split = true;
+          };
+
+          master = {
+            new_is_master = false;
+            mfact = "0.5";
+            orientation = "left";
+            smart_resizing = true;
+          };
+
+          binds = {
+            workspace_back_and_forth = true;
+          };
+
+          misc = {
+            # disable_hyprland_logo = true; TODO
+            # disable_splash_rendering = true;
+            mouse_move_enables_dpms = true;
+            # animate_manual_resizes = true;
+            # animate_mouse_windowdragging = true;
+            # key_press_enables_dpms = true;
+            enable_swallow = false;
+            swallow_regex = "^([Kk]itty|[Ww]ezterm)$";
+          };
+
+          # bind workspaces to monitors
+          workspace = lib.concatMap ({
+            name,
+            workspaces,
+            ...
+          }:
+            lib.forEach workspaces (ws: "${toString ws}, monitor:${name}"))
+          displays;
+
+          windowrulev2 = [
+            # "dimaround,floating:1"
+            "bordersize 5,fullscreen:1" # monocle mode
+            "float,class:(wlroots)" # hyprland debug session
+          ];
+
+          windowrule = [
+            # do not idle while watching videos
+            "idleinhibit fullscreen,Brave-browser"
+            "idleinhibit fullscreen,firefox-aurora"
+            "idleinhibit focus,YouTube"
+            "idleinhibit focus,mpv"
+          ];
+
+          exec-once = [
+            # clipboard manager
+            "wl-paste --watch cliphist store"
+          ];
+
+          # source = "~/.config/hypr/hyprland-test.conf";
+        }
+        # handle trackpad settings
+        (lib.optionalAttrs isLaptop {
+          gestures = {
+            workspace_swipe = true;
+          };
+
+          # handle laptop lid
+          bindl = [
+            # ",switch:on:Lid Switch, exec, hyprctl keyword monitor ${displayCfg.monitor1}, 1920x1080, 0x0, 1"
+            # ",switch:off:Lid Switch, exec, hyprctl monitor ${displayCfg.monitor1}, disable"
+            ",switch:Lid Switch, exec, hypr-lock"
+          ];
+        })
+      ];
+
+      # hyprland crash reports
+      rot.persist = {
+        home.directories = [
+          ".hyprland"
+        ];
+      };
+    };
   }
---
+++ home-manager/hyprland/startup.nix
@@ -0,0 +1,61 @@
+
+{
+  config,
+  lib,
+  pkgs,
+  user,
+  ...
+}: let
+  openOnWorkspace = workspace: program: "[workspace ${toString workspace} silent] ${program}";
+in {
+  config = lib.mkIf config.wayland.windowManager.hyprland.enable {
+    # start hyprland
+    rot.shell.profileExtra = ''
+      if [ "$(tty)" = "/dev/tty1" ]; then
+        exec Hyprland &> /dev/null
+      fi
+    '';
+
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        # init ipc listener
+        "hypr-ipc &"
+
+        # browsers
+        # (openOnWorkspace 1 "brave --incognito")
+        # (openOnWorkspace 1 "brave --profile-directory=Default")
+
+        # file manager
+        (openOnWorkspace 4 "nemo")
+
+        # terminal
+        (openOnWorkspace 1 "$term")
+
+        # firefox
+        # (openOnWorkspace 9 "firefox-developer-edition https://discordapp.com/channels/@me https://web.whatsapp.com http://localhost:9091")
+
+        # download desktop
+        # (openOnWorkspace 10 "$term nvim ~/Desktop/yt.txt")
+        # (openOnWorkspace 10 "$term")
+
+        # "${pkgs.swayidle}/bin/swayidle -w timeout 480 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'"
+
+        # focus the initial workspaces on startup
+        # "hyprctl dispatch workspace 9"
+        # "hyprctl dispatch workspace 7"
+        "hyprctl dispatch workspace 1"
+
+        # FIXME: weird race condition with swww init, need to sleep for a second
+        # https://github.com/Horus645/swww/issues/144
+        # "sleep 1; swww init && hypr-wallpaper"
+
+        # "sleep 5 && launch-waybar"
+
+        # fix gparted "cannot open display: :0" error
+        "${pkgs.xorg.xhost}/bin/xhost +local:${user}"
+        # fix Authorization required, but no authorization protocol specified error
+        # "${pkgs.xorg.xhost}/bin/xhost si:localuser:root"
+      ];
+    };
+  };
+}
