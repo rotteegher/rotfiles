73dee585e4a1ad017e74a6f122041bde97c80ce6
Sawako Taiga <noisetide@proton.me>
2024-01-03 23:41:54 +1100

    Fix omen host. Add reinstall ablity to install.sh

--- hosts/omen/default.nix
+++ hosts/omen/default.nix
@@ -1,12 +1,15 @@
 {user, ...}: {
   rot-nixos = {
-    kanata.enable = true;
+    # kanata.enable = false;
   };
 
   # by-id doesn't seem to work with amd mobo
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
 
   networking.hostId = "abb4d116"; # required for zfs
+
+  # Autologin
+  services.xserver.displayManager.autoLogin.user = user;
 
   # allow building and pushing of laptop config from desktop
   nix.settings.trusted-users = [user];
