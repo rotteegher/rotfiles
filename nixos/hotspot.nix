1a51d97057f166cd43178ef1bb10bfb862f781e2
Sawako Karin <noisetide@proton.me>
2024-01-18 19:40:02 +1100

    Add WiFi Hotspot capability

---
+++ nixos/hotspot.nix
@@ -0,0 +1,18 @@
+{config, lib, ...}:
+let
+cfg = config.rot-nixos.hotspot;
+in
+{
+  config = lib.mkIf cfg.enable {
+    # Enable WIFi Hotspot
+    services.create_ap = {
+      enable = true;
+      settings = {
+        INTERNET_IFACE = cfg.internet_iface;
+        WIFI_IFACE = cfg.wifi_iface;
+        SSID = cfg.ssid;
+        PASSPHRASE = cfg.passphrase;
+      };
+    };
+  };
+}
