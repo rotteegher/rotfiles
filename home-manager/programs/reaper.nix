fda1e92fb114b93e37a00bcaf852096a80ceb625
Sawako Karin <noisetide@proton.me>
2024-03-31 04:50:17 +1100

    Reaper stuff

--- home-manager/programs/reaper.nix
+++ home-manager/programs/reaper.nix
@@ -6,14 +6,37 @@
 }: {
   config = lib.mkIf config.custom.reaper.enable {
     home = {
-      packages = [pkgs.reaper pkgs.helm];
+      packages = with pkgs; [ 
+        reaper 
+        helm
+
+        oxefmsynth
+        aether-lv2
+        bespokesynth
+      ];
       # persist plugins
-      sessionVariables = {
-        LV2_PATH = "~/.nix-profile/lib/lv2/:~/.lv2:/nix/var/nix/profiles/default/lib/lv2:/var/run/current-system/sw/lib/lv2";
-        VST_PATH = "~/.nix-profile/lib/vst/:~/.vst:/nix/var/nix/profiles/default/lib/vst:/var/run/current-system/sw/lib/vst";
-        LXVST_PATH = "~/.nix-profile/lib/lxvst/:~/.lxvst:/nix/var/nix/profiles/default/lib/lxvst:/var/run/current-system/sw/lib/lxvst";
-        LADSPA_PATH = "~/.nix-profile/lib/ladspa/:~/.ladspa:/nix/var/nix/profiles/default/lib/ladspa:/var/run/current-system/sw/lib/ladspa";
-        DSSI_PATH = "~/.nix-profile/lib/dssi/:~/.dssi:/nix/var/nix/profiles/default/lib/dssi:/var/run/current-system/sw/lib/dssi";
+      # sessionVariables = {
+      #   LV2_PATH = "~/.nix-profile/lib/lv2/:~/.lv2:/nix/var/nix/profiles/default/lib/lv2:/var/run/current-system/sw/lib/lv2";
+      #   VST_PATH = "~/.nix-profile/lib/vst/:~/.vst:/nix/var/nix/profiles/default/lib/vst:/var/run/current-system/sw/lib/vst";
+      #   LXVST_PATH = "~/.nix-profile/lib/lxvst/:~/.lxvst:/nix/var/nix/profiles/default/lib/lxvst:/var/run/current-system/sw/lib/lxvst";
+      #   LADSPA_PATH = "~/.nix-profile/lib/ladspa/:~/.ladspa:/nix/var/nix/profiles/default/lib/ladspa:/var/run/current-system/sw/lib/ladspa";
+      #   DSSI_PATH = "~/.nix-profile/lib/dssi/:~/.dssi:/nix/var/nix/profiles/default/lib/dssi:/var/run/current-system/sw/lib/dssi";
+      # };
+      sessionVariables = let
+        makePluginPath = format:
+          (lib.makeSearchPath format [
+            "$HOME/.nix-profile/lib"
+            "/run/current-system/sw/lib"
+            "/etc/profiles/per-user/$USER/lib"
+          ])
+          + ":$HOME/.${format}";
+      in {
+        DSSI_PATH   = makePluginPath "dssi";
+        LADSPA_PATH = makePluginPath "ladspa";
+        LV2_PATH    = makePluginPath "lv2";
+        LXVST_PATH  = makePluginPath "lxvst";
+        VST_PATH    = makePluginPath "vst";
+        VST3_PATH   = makePluginPath "vst3";
       };
     };
     custom.persist = {
--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -11,7 +11,7 @@
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "usbcore"];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = ["kvm-intel" "usbcore"];
+  boot.kernelModules = ["kvm-intel" "usbcore" "snd-seq" "snd-rawmidi"];
   boot.blacklistedKernelModules = ["amdgpu"];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [
