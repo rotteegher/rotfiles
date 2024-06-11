d4a979afeecbbc03918902416442b777172e47ad
Sawako Karin <noisetide@proton.me>
2024-06-12 03:22:29 +1000

    Update

--- templates/rust-toolchain/flake.nix
+++ templates/rust-toolchain/flake.nix
@@ -144,7 +144,6 @@
             };
 
             env = {
-              CARGO_TARGET_DIR = null;
               LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH";
               XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
             };
