bccbd7097a89be974fd94cef96d31e5d37b58c8f
Sawako Taiga <noisetide@proton.me>
2025-07-11 05:37:49 +1000

    Implement file browsing sws static and swsb dynamic, all with routing

--- nixos/hotspot.nix
+++ nixos/hotspot.nix
@@ -8,6 +8,12 @@
 in {
   config = lib.mkIf (config.hm.custom.wifi.enable && cfg.enable) {
     environment.systemPackages = [ pkgs.linux-wifi-hotspot pkgs.hostapd ];
+
+    environment.etc."dnsmasq.d/create_ap.conf".text = ''
+      dhcp-range=192.168.12.100,192.168.12.254,12h
+      dhcp-host=64:c6:d2:4b:e8:1f,192.168.12.200
+    '';
+
     # Enable WIFi Hotspot
     services.create_ap = {
       enable = true;
@@ -16,6 +22,7 @@ in {
         WIFI_IFACE = cfg.wifi_iface;
         SSID = cfg.ssid;
         PASSPHRASE = cfg.passphrase;
+        DNSMASQ_CONF_FILE = "/etc/dnsmasq.d/create_ap.conf";
       };
     };
   };
--- nixos/sws.nix
+++ nixos/sws.nix
@@ -7,18 +7,12 @@
 }: let
   cfg = config.custom.static-web-server;
   domain-name = "rotteegher.ddns.net";
-in {
+  path-to-serve = "/md/wdc-data/_SMALL/_ONLINE_TANK";
+in lib.mkMerge [
+{
   security.acme = {
     acceptTerms = true;
     defaults.email = "noisetide@proton.me";
-    # certs.${domain-name} = {
-    #   reloadServices = [ "static-web-server" ];
-    #   webroot = null;  # <-- manually disable it to avoid conflicting methods
-    #   listenHTTP = ":80";
-    #   group = "www-data";
-    #   # EC is not supported by SWS versions before 2.16.1
-    #   keyType = "rsa4096";
-    # };
   };
 
   # Now we need to open port 80 for the ACME challenge and port 443 for SWS itself
@@ -27,34 +21,84 @@ in {
   # Configure SWS to use the generated TLS certs
   services.static-web-server = lib.mkIf cfg.enable {
     enable = true;
-    root = "/md/wdc-data/_SMALL/_ONLINE_TANK";
+    root = path-to-serve;
     listen = cfg.listen;
     configuration = {
       general = { 
         directory-listing = true;
-        log-level = "trace";
-        # http2 = true;
-        # Edit the domain name in the file to match your real domain name as configured in the ACME settings
-        # http2-tls-cert = "/var/lib/acme/${domain-name}/fullchain.pem";
-        # http2-tls-key = "/var/lib/acme/${domain-name}/key.pem";
-        # Info here: https://static-web-server.net/features/security-headers/
-        # This option is only needed for versions prior to 2.18.0, after which it defaults to true
+        directory-listing-order = 1;
+        directory-listing-format = "html";
+        directory-listing-download = [];
+
         security-headers = true;
+        log-level = "trace";
+        #### Log request Remote Address if available
+        log-remote-address = true;
+        # #### Log real IP from X-Forwarded-For header if available
+        log-forwarded-for = true;
+
+        redirect-trailing-slash = true;
+        health = false;
+
+        cache-control-headers = true;
+        compression = true;
+        compression-level = "default";
+        compression-static = true;
+
+        maintenance-mode = false;
       };
     };
   };
 
+    # Ensure FileBrowser is available
+  environment.systemPackages = [ pkgs.filebrowser ];
+
+  # Define the systemd service for FileBrowser
+  systemd.services.filebrowser = {
+    description = "FileBrowser Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.filebrowser}/bin/filebrowser -d /var/lib/filebrowser/filebrowser.db --username ${user} --password $SERVER_AUTH --baseurl /filebrowser -r ${path-to-serve} -p 3333";
+      WorkingDirectory = path-to-serve;
+      StateDirectory = "filebrowser";
+      Restart = "on-failure";
+      RestartSec = 5;
+      User = user;
+      Group = "users";
+    };
+  };
+
+
   # Now we need to override some things in the systemd unit files to allow access to those TLS certs, starting with creating a new Linux group:
-  # users.groups.www-data = {};
+  users.groups.www-data = {};
   # This strategy can be useful to override other advanced features as-needed
-  # systemd.services.static-web-server.serviceConfig.SupplementaryGroups = pkgs.lib.mkForce [ "" "www-data" ];
+  systemd.services.static-web-server.serviceConfig.SupplementaryGroups = pkgs.lib.mkForce [ "" "www-data" ];
   # Note that "/some/path" should match your "root" option
-  # systemd.services.static-web-server.serviceConfig.BindReadOnlyPaths = pkgs.lib.mkForce [
-  #   # "/some/path"
-  #   "/var/lib/acme/${domain-name}"
-  # ];
+  systemd.services.static-web-server.serviceConfig.BindReadOnlyPaths = pkgs.lib.mkForce [
+    # "/some/path"
+    "/var/lib/acme/${domain-name}"
+  ];
 
   custom.persist = {
-    root.directories = ["/var/lib/acme"];
+    root.directories = ["/var/lib/acme" "/var/lib/filebrowser"];
   };
 }
+(lib.mkIf config.custom.sops.enable {
+      sops.secrets.sws.owner = user;
+      sops.secrets.swsb.owner = user;
+
+      systemd.services.static-web-server = {
+        serviceConfig = {
+          EnvironmentFile = config.sops.secrets.sws.path;
+        };
+      };
+
+      systemd.services.filebrowser = {
+        serviceConfig = {
+          EnvironmentFile = config.sops.secrets.swsb.path;
+        };
+      };
+
+  })
+]
